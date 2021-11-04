5492ec924343d59095b6500aacbb065f2c0aee79
Adrian Lopez Iglesias <adrianlopeziglesias1@gmail.com>
2021-11-04 15:14:13 -0300

    FrontEnd ITPatagonia Challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherWidgetComponent } from './components/weather-widget.component';
+// import { WebsocketService } from './services/websocket.service';
+// import { WeatherService } from './services/weather.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/weather-widget.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { webSocket } from "rxjs/webSocket";
+import { map } from 'rxjs/operators';
+const subject = webSocket({
+	url: "ws://localhost:3500",
+	deserializer: data => data
+});
+
+
+
+
+
+@Component({
+	selector: 'weather-widget',
+	template: `
+		<p>El clima en 								 {{weather.name}}</p>
+		<p>{{weather.weather[0].main}}</p>
+		<p>Temperatura							 : {{weather.main.temp}}</p>
+		<p>Sensación   térmica       : {{weather.main.feels_like}}</p>
+		<p>Temperatura mínima        : {{weather.main.temp_min}}</p>
+		<p>Temperatura máxima        : {{weather.main.temp_max}}</p>
+		<p>Presión     atmosferica   : {{weather.main.pressure}}</p>
+		<p>Humedad    							 : {{weather.main.humidity}}</p>
+		<p>Visibilidad 							 : {{weather.visibility}}</p>
+		<p>Viento dirección					 : {{weather.wind.deg}}</p>
+		<p>Viento velocidad					 : {{weather.wind.speed}}</p>
+	`,
+	providers: []
+})
+export class WeatherWidgetComponent implements OnInit {
+	constructor() { 
+	}
+	weather = {
+		name: 'Esperando datos del servidor',
+		weather: [{ main: 'Esperando datos del servidor' }],
+		main: {
+			temp: 'Esperando datos del servidor',
+			feels_like: 'Esperando datos del servidor',
+			temp_min: 'Esperando datos del servidor',
+			temp_max: 'Esperando datos del servidor',
+			pressure: 'Esperando datos del servidor',
+			humidity: 'Esperando datos del servidor',
+		},
+		visibility: 'Esperando datos del servidor',
+		wind: {
+			deg: 'Esperando datos del servidor',
+			speed: 'Esperando datos del servidor'
+		}
+	}
+
+
+
+	ngOnInit(): void {
+		
+
+		subject.subscribe(
+			(data) => this.setWeather(JSON.parse(data.data)),
+			(err) => console.log(err),
+			() => console.log('complete')
+		);
+		let message = { op: 'Start' } as unknown as MessageEvent<any>
+		subject.next(message);
+	}
+
+
+
+	setWeather(data) {
+		this.weather = {
+			name: data.name,
+			weather: [{ main: data.weather[0].main +" "+ data.weather[0].description }],
+			main: {
+				temp: (data.main.temp) + 'º',
+				feels_like: (data.main.feels_like) + 'º',
+				temp_min: (data.main.temp_min) + 'º',
+				temp_max: (data.main.temp_max) + 'º',
+				pressure: data.main.pressure + " hPa",
+				humidity: data.main.humidity + "%",
+			},
+			visibility: data.visibility + " km",
+			wind: {
+				deg: data.wind.deg + "º",
+				speed: data.wind.speed + " kmh"
+			}
+		}
+		console.log(data);
+	}
+
+}
